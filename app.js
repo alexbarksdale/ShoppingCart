88a500dc0ec549be3a6a393c6c3f6a45f5dfacbb
alexbarksdale <ale.barksdale@gmail.com>
2019-11-11 14:22:50 -0800

    [Remove] EventHandler.js

--- app.js
+++ app.js
@@ -8,6 +8,41 @@ const itemList = document.getElementById('item-list');
 // Holds the items in the cart
 const cart = [];
 
+// Adds item to list
+addForm.onsubmit = event => {
+    event.preventDefault();
+    const name = itemName.value;
+    const price = itemPrice.value;
+
+    addItem(name, price);
+};
+
+// Handle clicks on list
+itemList.onclick = event => {
+    if (event.target && event.target.classList.contains('remove')) {
+        //                       ..dataset.(x)   x = data-(x)=""
+        const name = event.target.dataset.name; // data-name=""
+
+        removeItem(name);
+    } else if (event.target && event.target.classList.contains('add-one')) {
+        const name = event.target.dataset.name;
+        addItem(name);
+    } else if (event.target && event.target.classList.contains('remove-one')) {
+        const name = event.target.dataset.name;
+        removeItem(name, 1);
+    }
+};
+
+// Handle change events on update input
+itemList.onchange = event => {
+    if (event.target && event.target.classList.contains('update')) {
+        const name = event.target.dataset.name;
+        const quantity = parseInt(event.target.value);
+
+        updateCart(name, quantity);
+    }
+};
+
 // Adds item to cart
 function addItem(name, price) {
     const item = {
